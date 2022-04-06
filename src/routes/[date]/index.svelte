5d8df39e19a373c10881f26c8f5a96647d2414a0
gaylonalfano <gaylon.alfano@gmail.com>
2022-04-06 13:23:58 +0800

    Working on getting some basic routing in place. Would like to hit a YYYY-MM-DD route and it lists all projects, if any. Actually, probably should be /data/projects route that lists all projects for that date. Will have a dynamic id route for each individual project as well.

---
+++ src/routes/[date]/index.svelte
@@ -0,0 +1,42 @@
+<script context="module">
+	export async function load({ url, params }) {
+		try {
+			// Dynamic import
+			// NOTE Use .default to grab the ProxyComponent. Remove it and you have Module {}
+			// that holds both default AND metadata objects (where Frontmatter is stored)
+			console.log(`url: ${url}`);
+			console.log(`params: ${params}`);
+			console.log(`date: ${params.date}`);
+			return {
+				props: {
+					url,
+					date: params.date
+				}
+			};
+		} catch (e) {
+			console.log(e);
+			// NOTE Can redirect to a valid URL using redirect and status 303
+			// return {
+			// 	status: 303,
+			// 	redirect: '/posts'
+			// };
+			// NOTE Or we can return a 404
+			// NOTE Whatever we set here will get passed into our __error.svelte
+			// load({error, status}) function!
+			return {
+				status: 404,
+				error: 'Date not found'
+			};
+		}
+	}
+</script>
+
+<script>
+	export let date;
+	export let url;
+</script>
+
+<h1>~/[date]</h1>
+<p>This should list all projects for this individual date.</p>
+<h2>{url}</h2>
+<h2>{date}</h2>
---
+++ src/routes/[date]/projects/index.svelte
@@ -0,0 +1,31 @@
+<script context="module">
+	// Can use Svelte SSR to first fetch the matching pokemon data
+	// NOTE This gives us the ability to query and load all the data
+	// BEFORE anything is rendered! This is different vs Store!
+	// NOTE Using a Store will first load the page, query the data, and
+	// then displaying it. Also, a Store gives us a Writable (similar to Ref)
+	// where we use the $ symbol to access. When using context="module",
+	// we don't need $ anymore since we can access data via props object
+	export async function load({ url, params }) {
+		// load() runs before rendering the content of the page
+		// Want to make a fetch() to the API using pokemon id, which
+		// is available from page.params object
+		// const id = page.params.id;
+		// const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+		// const response = await fetch(url);
+		// const pokeman = await response.json();
+
+		// Now return Object with props property and add our pokeman data
+		// NOTE Again, this all runs BEFORE Svelte renders anything!
+		return { props: { url, id: params.id } };
+	}
+</script>
+
+<script>
+	export let url;
+	export let id;
+</script>
+
+<h1>~/[date]/projects/[id]</h1>
+<h2>{url}</h2>
+<h2>{id}</h2>

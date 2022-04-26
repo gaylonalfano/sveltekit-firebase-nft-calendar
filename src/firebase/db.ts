ef024b90f76ab0eb7b2040fb7ed5aeadc787c8f6
gaylonalfano <gaylon.alfano@gmail.com>
2022-04-26 08:58:41 +0800

    Updated packages.

---
+++ src/firebase/db.ts
@@ -0,0 +1,9 @@
+import { collection, getDocs } from 'firebase/firestore';
+import { getFirebase } from './firebase';
+// import type { Project } from './models';
+
+// export async function getProjects(): Promise<Record<string, any>[]> {
+// 	const { db } = getFirebase();
+// 	const colRef = collection(db, 'projects');
+// 	const snapshot = await getDocs(colRef);
+// }
